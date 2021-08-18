83e7e5d2997d365ae99703a83064ac1961ab9634
kandula-prakash <abhinayprakash143@gmail.com>
2021-08-18 13:06:43 +0530

    feat:day11&12

---
+++ FileReader.cs
@@ -0,0 +1,27 @@
+﻿//using Newtonsoft.Json;
+//using System;
+//using System.Collections.Generic;
+
+//using System.IO;
+
+
+//namespace Day11And12Problems
+//{
+//   public class FileReader
+//    {
+//        public static List<Inventory> inventories = new List<Inventory>();
+
+//        public object JsonConvert { get; private set; }
+
+//        public List<Inventory> GetInventry()
+//        {
+//            string text = File.ReadAllText("C:\\Users\\shant\\Documents\\Data.json");
+//            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+//            return inventories;
+//        }
+
+//    }
+//}
+
+    
+
---
+++ Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day11And12Problems
+{
+   public class Inventory
+    {
+         
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Price { get; set; }
+    }
+}
+    
+
